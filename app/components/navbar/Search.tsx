c87b0ac4d96c1ff06264dadaf6e7d83e9d275bb7
Anoter <prosidegcr@gmail.com>
2023-04-17 12:21:15 -0300

    adding Search

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,22 @@
+"user Client";
+
+const Search = () => {
+    return ( 
+        <div className="
+            border[1px]
+            w-full
+            md:w-auto
+            py-2
+            rounded-full
+            shadow-sm
+            hover:shadow-md
+            trasition
+            cursor-pointer
+        "
+        >
+
+        </div>
+     );
+}
+ 
+export default Search;
