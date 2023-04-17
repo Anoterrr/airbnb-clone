8d3a540933a56f9004cab42e63b29c91eeb808b9
Anoter <prosidegcr@gmail.com>
2023-04-17 12:20:36 -0300

    navBar update

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../container"
 import Logo from "./Logo"
+import Search from "./Search"
 
 const Navbar = () => {
   return (
@@ -20,6 +21,7 @@ const Navbar = () => {
             md:gap-0
           ">
             <Logo />
+            <Search />
           </div>
         </Container>
       </div>
